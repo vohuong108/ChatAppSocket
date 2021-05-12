3c7d87aad3153878d4b594fea306e5e813e1bcf1
trung3110 <tn0009592@gmail.com>
2021-05-12 07:42:55 +0700

    ver2

--- src/server/Server.java
+++ src/server/Server.java
@@ -13,17 +13,7 @@ import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-/**
- * A multithreaded chat room server. When a client connects the server requests
- * a screen name by sending the client the text "SUBMITNAME", and keeps
- * requesting a name until a unique one is received. After a client submits a
- * unique name, the server acknowledges with "NAMEACCEPTED". Then all messages
- * from that client will be broadcast to all other clients that have submitted a
- * unique screen name. The broadcast messages are prefixed with "MESSAGE".
- *
- * This is just a teaching example so it can be enhanced in many ways, e.g.,
- * better logging. Another is to accept a lot of fun commands, like Slack.
- */
+
 public class Server {
 
     // All client names, so we can check for duplicates upon registration.
@@ -32,30 +22,19 @@ public class Server {
     // The set of all the print writers for all the clients, used for broadcast.
     private static Set<PrintWriter> writers = new HashSet<>();
 
-    /**
-     * The client handler task.
-     */
+
     private static class Handler implements Runnable {
         private String name;
         private Socket socket;
         private Scanner in;
         private PrintWriter out;
 
-        /**
-         * Constructs a handler thread, squirreling away the socket. All the interesting
-         * work is done in the run method. Remember the constructor is called from the
-         * server's main method, so this has to be as short as possible.
-         */
+
         public Handler(Socket socket) {
             this.socket = socket;
         }
 
-        /**
-         * Services this thread's client by repeatedly requesting a screen name until a
-         * unique one has been submitted, then acknowledges the name and registers the
-         * output stream for the client in a global set, then repeatedly gets inputs and
-         * broadcasts them.
-         */
+
         public void run() {
             try {
                 in = new Scanner(socket.getInputStream());
@@ -76,16 +55,14 @@ public class Server {
                     }
                 }
 
-                // Now that a successful name has been chosen, add the socket's print writer
-                // to the set of all writers so this client can receive broadcast messages.
-                // But BEFORE THAT, let everyone else know that the new person has joined!
+
                 out.println("NAMEACCEPTED " + name);
                 for (PrintWriter writer : writers) {
                     writer.println("MESSAGE " + name + " has joined");
                 }
                 writers.add(out);
 
-                // Accept messages from this client and broadcast them.
+
                 while (true) {
                     String input = in.nextLine();
                     if (input.toLowerCase().startsWith("/quit")) {
@@ -118,7 +95,7 @@ public class Server {
 
     public static void main(String[] args) throws Exception {
         System.out.println("The chat server is running...");
-        ExecutorService pool = Executors.newFixedThreadPool(500);
+        ExecutorService pool = Executors.newFixedThreadPool(2);
         try (ServerSocket listener = new ServerSocket(59001)) {
             while (true) {
                 pool.execute(new Handler(listener.accept()));
